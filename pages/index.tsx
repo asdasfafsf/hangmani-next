db16a663507cb06cbff0767b43b66a3dae3e37d6
asdasfafsf <asdasfafsf@naver.com>
2023-02-01 19:20:12 +0900

    etc: styled => emotion으로 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
-import styled from 'styled-components'
+import styled from '@emotion/styled'
 
 const Layout = styled.div`
   background-color: pink;

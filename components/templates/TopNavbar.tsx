8b94cb9fb923a0589819b60ed61ecad80ff2a4ce
asdasfafsf <asdasfafsf@naver.com>
2023-02-04 19:21:04 +0900

    feat: header design ~ing

--- components/templates/TopNavbar.tsx
+++ components/templates/TopNavbar.tsx
@@ -3,6 +3,7 @@ import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 import Logo from "../atoms/Logo";
 import TopView from "./TopView";
+import Link from "next/link";
 
 interface TopNavBar {
    
@@ -17,28 +18,39 @@ const NavWrap = styled.div`
     height: 700px;
 `
 
-
 const dynamicNavStyle = (props:NavProps) => css`
-    transition: 0.15s;
     background: ${(props.isMerge ? '#707070' : '#FFFFFF')};
+`
+const Nav = styled.nav<NavProps>`
+    ${dynamicNavStyle}
     width: 100%;
     height: 100px;
     margin: 0px;
     position: fixed;
     z-index: 50;
-`
-const Nav = styled.nav<NavProps>`
-    ${dynamicNavStyle}
+    display: flex;
+    transition: 0.15s;
 `
 
 const Ul = styled.ul`
+    display: flex;
     list-style: none;
     margin: 0px;
+    height: 100%;
+    align-items: center;
+    justify-content: center;
 `
 
 const Li = styled.li`
     margin: 0px;
+
+    a {
+        color: black;
+    }
 `
+
+
+
 
 const TopNavBar = ({}: TopNavBar) => {
     const { header } = useLayout()
@@ -47,9 +59,9 @@ const TopNavBar = ({}: TopNavBar) => {
             <Nav isMerge={header}>
                 <Logo />
                     <Ul>
-                        <Li>
-
-                        </Li>
+                        <Li><Link href={"#"}>로그인</Link></Li>
+                        <Li><Link href={"#"}>회원가입</Link></Li>
+                        
                     </Ul>
             </Nav>
             <TopView />

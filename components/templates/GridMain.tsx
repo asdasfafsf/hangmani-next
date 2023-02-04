5a967a42943e16f0bfb6a3f24719f7552779cf95
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-02-04 12:53:59 +0900

    etc: GridMain 수정

--- components/templates/GridMain.tsx
+++ components/templates/GridMain.tsx
@@ -11,7 +11,7 @@ const GridMain = () => {
     return (<GridWrapper>
         {
             Array.from({length : 30}, () => 1)
-                .map(elem => <Card />)
+                .map(() => <Card />)
         }
 
 

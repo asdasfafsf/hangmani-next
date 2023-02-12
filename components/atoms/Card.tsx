75350a43a63f5eff4151fb03a28c3bb4877b3ee6
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-02-12 18:15:59 +0900

    feat: LottoRank 분리

--- components/atoms/Card.tsx
+++ components/atoms/Card.tsx
@@ -1,8 +1,7 @@
 import styled from "@emotion/styled";
 import Image from "next/image";
 import MapImagePNG from 'public/images/map.png';
-import Rank2 from 'public/svgs/icon_ranking.svg';
-import Rank1 from 'public/svgs/icon_ranking1.svg';
+import { LottoRank } from "./LottoRank";
 
 interface CardProps {
     rank: number;
@@ -41,37 +40,14 @@ const CardImg = styled(Image)`
 
 
 const LottoRankWrapper = styled.div`
-    display: flex;
-    align-items: flex-end;
-    padding: 8px 16px;
-    gap: 8px;
-
     position: absolute;
     left: 62.5%;
     right: 6.94%;
     top: 6.79%;
     bottom: 80.3%;
-    width: 58px;
-
-    background: #FFFFFF;
-    border-radius: 6px;
-    box-shadow: 1px 4px 4px 1px gray;
-`
-
-const LottoRankText = styled.p`
-    display: flex;
-    /* align-items: flex-end; */
-    /* justify-content: center; */
     margin: 0px;
-    width: 32px;
-    height: 100%;
-    font-family: 'Noto Sans KR';
-    font-style: normal;
-    font-weight: 700;
-    font-size: 16px;
-    letter-spacing: -0.05em;
-
-    color: #333333;
+    padding: 0px;
+    box-sizing: border-box;
 `
 
 const StoreName = styled.div`
@@ -102,8 +78,7 @@ const Card = ({rank, winCount, thumbnail, storeName}: CardProps) => {
             <CardContainer>
                 
                 <LottoRankWrapper>
-                    {rank === 2 ? <Rank2 /> : <Rank1 />}
-                    <LottoRankText>{winCount}회</LottoRankText>
+                    <LottoRank rank={rank} winCount={winCount}/>
                 </LottoRankWrapper>
                 <CardImageWrapper>
                     <CardImg 
---
+++ components/atoms/LottoRank.tsx
@@ -0,0 +1,54 @@
+import styled from "@emotion/styled";
+import Rank2 from 'public/svgs/icon_ranking.svg';
+import Rank1 from 'public/svgs/icon_ranking1.svg';
+
+interface LottoRankProps {
+    winCount: number;
+    rank: number;
+}
+
+
+const LottoRankWrapper = styled.div`
+    position: absolute;
+    display: flex;
+    justify-content: flex-start;
+    align-item: center;
+    padding: 3px 16px;
+    gap: 8px;
+    /* width: 58px; */
+    background: #FFFFFF;
+    border-radius: 6px;
+    box-shadow: 1px 4px 4px 1px gray;
+`
+
+const LottoRankTextWrapper = styled.div`
+    display: flex;
+    align-item: center;
+    justify-content: center;
+`
+
+
+const LottoRankText = styled.div`
+    align-items: center;
+    margin: 0px;
+    padding: 0px;
+    width: 32px;
+    height: 100%;
+    font-family: 'Noto Sans KR';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    letter-spacing: -0.05em;
+    margin-top: 5px;
+    color: #333333;
+`
+
+export const LottoRank = ({winCount, rank}: LottoRankProps) => {
+
+    return (<LottoRankWrapper>
+                {rank === 2 ? <Rank2 /> : <Rank1 />}
+                <LottoRankText>
+                    <div>{winCount}회</div>
+                </LottoRankText>
+    </LottoRankWrapper>)
+}

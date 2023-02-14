550515207493db2da3fee56cef7479f56477f4ab
asdasfafsf <asdasfafsf@naver.com>
2023-02-14 19:39:10 +0900

    feat: 좌측 리스트 추가

--- components/templates/MapTemplate.tsx
+++ components/templates/MapTemplate.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import StoreCard from "../atoms/StoreCard";
 import FilterSelector from "../patterns/FilterSelector";
 
 
@@ -18,7 +19,7 @@ const Aside = styled.aside`
     
     width: 450px;
     height: 100%;
-    border: 1px solid black;
+    /* border: 1px solid black; */
 `
 
 const MainTemplate = () => {
@@ -30,10 +31,42 @@ const MainTemplate = () => {
             <Aside>
                 <FilterSelector />
                 <FilterSelector />
-
+                <StoreCard 
+                    oneWinCount={10}
+                    twoWinCount={25}
+                    storeName={"나눔로또판매점"} 
+                    distance={"50m"} 
+                    storeAddr={"서울특별시 영등포구 영등포동가 10-13"} 
+                    lottoTypeList={['lotto6/45', 'speeto', 'speeto2000']} 
+                />
+                <StoreCard 
+                    oneWinCount={10}
+                    twoWinCount={25}
+                    storeName={"나눔로또판매점"} 
+                    distance={"50m"} 
+                    storeAddr={"서울특별시 영등포구 영등포동가 10-13"} 
+                    lottoTypeList={['lotto6/45', 'speeto', 'speeto2000']} 
+                />
+                <StoreCard 
+                    oneWinCount={10}
+                    twoWinCount={25}
+                    storeName={"나눔로또판매점"} 
+                    distance={"50m"} 
+                    storeAddr={"서울특별시 영등포구 영등포동가 10-13"} 
+                    lottoTypeList={['lotto6/45', 'speeto', 'speeto2000']} 
+                />
+                <StoreCard 
+                    oneWinCount={10}
+                    twoWinCount={25}
+                    storeName={"나눔로또판매점"} 
+                    distance={"50m"} 
+                    storeAddr={"서울특별시 영등포구 영등포동가 10-13"} 
+                    lottoTypeList={['lotto6/45', 'speeto', 'speeto2000']} 
+                />
             </Aside>
                 
             <Section>
+                
             </Section>
         </Wrapper>
     )

87fb4f230ccab86baf80ae60f7cc2d67cd614c47
asdasfafsf <31441473+asdasfafsf@users.noreply.github.com>
2023-03-01 23:04:23 +0900

    feat: 네이버맵 디자인 진행중

--- components/patterns/NaverMaps.tsx
+++ components/patterns/NaverMaps.tsx
@@ -2,6 +2,7 @@ import useCurrentGeo from "@/hooks/useCurrentGeo";
 import useNaverMaps from "@/hooks/useNaverMaps";
 import styled from "@emotion/styled";
 import { useEffect, useRef, useState } from "react";
+import SquareButton from "../atoms/SquareButton";
 
 
 
@@ -13,9 +14,40 @@ const MapWrapper = styled.div`
 `
 
 const Map = styled.div`
+    position: relative;
     width: 100%;
     height: 100%;
 `
+
+
+const ReloadWrapper = styled.div`
+    position: absolute;
+    width: 130px;
+    height: 43px;
+    left: 1230px;
+    top: 60px;
+`
+
+const CurrentLocateWrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    /* padding: 10px 20px; */
+    /* box-sizing: border-box; */
+    gap: 10px;
+
+    position: absolute;
+    width: 257px;
+    height: 43px;
+    left: 30px;
+    top: 60px;
+
+    background: #FFFFFF;
+    border: 1px solid #D9D9D9;
+    border-radius: 4px;
+`
+
 
 const NaverMaps = () => {
     const mapElem = useRef(null);
@@ -39,20 +71,24 @@ const NaverMaps = () => {
         }
     },[])
 
-    useEffect(() => {   
-        if (naverMap) {
-
-        }
-    }, [naverMap])
-
 
 
     return (
         <MapWrapper>
             <Map id='map' ref={mapElem}>
-
             </Map>
-
+            <CurrentLocateWrapper>
+                현 위치 : 경기 성남시 분당구 야탑동
+            </CurrentLocateWrapper>
+            <ReloadWrapper>
+                    <SquareButton 
+                        width="130px"
+                        height="43px"
+                        color="#FF7044;"
+                        textColor="white"
+                        text="이 지역 재검색"
+                    />
+            </ReloadWrapper>
         </MapWrapper>
     )
 }

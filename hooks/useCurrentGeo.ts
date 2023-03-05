f7efeb21db041d90f42cadf660536743d0326f95
asdasfafsf <asdasfafsf@naver.com>
2023-03-05 16:28:04 +0900

    feat: 현위치로 설정 기능 추가

--- hooks/useCurrentGeo.ts
+++ hooks/useCurrentGeo.ts
@@ -1,3 +1,4 @@
+import { hasAuthority, hasGeolocation, getGeo } from "@/lib/geo";
 import { CurrentGeoAtom } from "@/recoils/CurrentGeo";
 import { useEffect } from "react";
 import { useRecoilState } from "recoil";
@@ -7,8 +8,23 @@ export const useCurrentGeo = () => {
     const [currentGeo, setCurrentGeo] = useRecoilState(CurrentGeoAtom);
 
 
+    const handleCurrentGeo = async () => {
+        if (hasGeolocation() === false) {
+            alert('GPS를 사용 불가능 한 상태입니다. 최신 브라우저에서 접속해주세요.');
+            return;
+        }
+
+        if (await hasAuthority() === false) {
+            alert('위치권한이 없습니다. 권한을 허용해주세요');
+            return false;
+        }
+
+        const geo = await getGeo();
+        setCurrentGeo(geo);
+    }
+
     
-    return [currentGeo, setCurrentGeo] as const;
+    return [currentGeo, handleCurrentGeo] as const;
 };
 
 
